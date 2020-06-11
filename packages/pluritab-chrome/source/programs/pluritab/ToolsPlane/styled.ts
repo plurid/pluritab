735aa41053b47da33616b378fc6e086a2cd01229
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-06-11 21:08:11 +0300

    pluritab-chrome > dev: ToolsPlane, tools

--- packages/pluritab-chrome/source/programs/pluritab/ToolsPlane/styled.ts
+++ packages/pluritab-chrome/source/programs/pluritab/ToolsPlane/styled.ts
@@ -3,5 +3,6 @@ import styled from 'styled-components';
 
 
 export const StyledToolsPlane: any = styled.div`
-
+    font-family: Ubuntu, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+        Roboto, Oxygen, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 `;
--- packages/pluritab-chrome/source/programs/pluritab/tools/CurrentIP/styled.ts
+++ packages/pluritab-chrome/source/programs/pluritab/tools/CurrentIP/styled.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components';
 
 
 export const StyledCurrentIP: any = styled.div`
-
+    padding: 1rem;
 `;
