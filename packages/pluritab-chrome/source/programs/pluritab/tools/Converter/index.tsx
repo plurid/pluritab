e22af962acd741dfc50bf97b92b935a42b065285
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-06-12 07:04:38 +0300

    pluritab-chrome > dev: Settings, tools

---
+++ packages/pluritab-chrome/source/programs/pluritab/tools/Converter/index.tsx
@@ -0,0 +1,25 @@
+import React, {
+    useState,
+    useEffect,
+} from 'react';
+
+import {
+    StyledConverter,
+} from './styled';
+
+
+
+interface ConverterProperties {
+}
+
+const Converter: React.FC<ConverterProperties> = () => {
+
+    return (
+        <StyledConverter>
+
+        </StyledConverter>
+    );
+}
+
+
+export default Converter;
---
+++ packages/pluritab-chrome/source/programs/pluritab/tools/Desearcher/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+
+
+export const StyledDesearcher: any = styled.div`
+    width: 400px;
+    padding: 2rem 0;
+    display: grid;
+    justify-items: center;
+`;
