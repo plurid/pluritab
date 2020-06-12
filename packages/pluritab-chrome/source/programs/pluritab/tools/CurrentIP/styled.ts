e3b2e3230cce1a95c581de5eaf5c5df84abbf6cb
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-06-12 20:36:06 +0300

    pluritab-chrome > dev: CurrentIP, Desearcher

--- packages/pluritab-chrome/source/programs/pluritab/tools/CurrentIP/styled.ts
+++ packages/pluritab-chrome/source/programs/pluritab/tools/CurrentIP/styled.ts
@@ -2,8 +2,17 @@ import styled from 'styled-components';
 
 
 
-export const StyledCurrentIP: any = styled.div`
+export const StyledCurrentIPContainer: any = styled.div`
     position: absolute;
     bottom: 1rem;
     left: 1rem;
+`;
+
+
+export const StyledCurrentIP: any = styled.div`
+    font-size: 0.9rem;
+    display: grid;
+    align-items: center;
+    justify-items: center;
+    grid-template-columns: auto 2rem;
 `;
